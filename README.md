12a498beaaa0efd1078ec4e41671dffb7c04e6da
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2022-12-24 04:38:07 -0800

    project init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Calculator
+Simple browser calculator.
+
+Based on the [Calculator Project](https://www.theodinproject.com/lessons/foundations-calculator) 
+from The Odin Project, but rewritten in `.ts` Typescript and `.scss` Sass.
+
+[Link to Live Site 🌐](https://gesty.dev/calculator)
+
+![Tech Stack](https://skillicons.dev/icons?i=ts,html,scss)
+
---
+++ js/script.ts
@@ -0,0 +1,113 @@
+// Calculator
+const text: string = 'by: gesty 🏄';
+console.log(text);
+
+/* TODO
+  * add keyboard event listeners
+    * switch cases?
+  * fix errors in 'equals' button
+  * round answer on display overflow
+  * refactor
+*/
+
+// Basic math functions
+function add(a: number, b:number) {
+  return a + b;
+}
+function subtract(a:number, b:number) {
+  return a - b;
+}
+function multiply(a:number, b:number) {
+  return a * b;
+}
+function divide(a:number, b:number) {
+  // if trying to divide by zero
+  if (b === 0) {
+    return "Nice try Satan..."
+  } else {
+    return a / b;
+  }
+}
+
+// Function to handle operations
+function operate(a:number, b:number, op:string) {
+  let result;
+  if (op == 'add') {
+    result = add(a, b);
+  } else if (op == 'subtract') {
+    result = subtract(a, b);
+  } else if (op == 'multiply') {
+    result = multiply(a, b);
+  } else {
+    result = divide(a, b);
+  }
+  return result;
+}
+
+// Result display div
+const resultsDiv = document.querySelector('.results');
+
+let tempNumber = ''; // allows for numbers >= 10
+// Saving inputs into arrays
+let numberList: number[] = [];
+let operatorList: string[] = [];
+
+
+if (resultsDiv !== null) {
+  // Event listeners for number buttons
+  const numberButtons = document.querySelectorAll('.number');
+  numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      tempNumber += button.innerHTML; // add number to tempNumber
+      resultsDiv.append(button.innerHTML); // add number to display
+    });
+  });
+
+  // Event listeners for operator buttons
+  const operatorButtons = document.querySelectorAll('.operator');
+  operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      numberList.push(parseInt(tempNumber)); // add tempNumber to numberList
+      operatorList.push(button.id) // add operator to operatorList
+      resultsDiv.append(` ${button.innerHTML} `); // add operator to display
+      tempNumber = ''; // reset tempNumber
+    });
+  });
+  
+  // Event listener on 'clear' button
+  const clearButton = document.querySelector('.clear');
+  if (clearButton !== null) {
+    clearButton.addEventListener('click', () => {
+      // resets everything back to default / blank
+      resultsDiv.textContent = '';
+      tempNumber = '';
+      numberList = [];
+      operatorList = [];
+    });
+  };
+
+  // Event listener on 'equals' button
+  const equalsButton = document.querySelector('.equals');
+  if (equalsButton !== null) {
+    equalsButton.addEventListener('click', () => {
+      numberList.push(parseInt(tempNumber));
+      
+      // while loop to allow multiple operations in single equation
+      while (numberList.length > 2) {
+        let tempResult = operate(numberList[0], numberList[1], operatorList[0]);
+        numberList.shift(); // removes first array item
+        operatorList.shift(); // removes first array item
+        numberList[0] = tempResult; // sets tempResult as new first array item
+      };
+
+      // Final result when number array only has 2 items left
+      let finalResult = operate(numberList[0], numberList[1], operatorList[0]);
+      resultsDiv.textContent = finalResult.toString();
+      tempNumber = finalResult; // sets solution to new tempNumber
+
+      // reset back to blank defaults
+      numberList = [];
+      operatorList = [];
+    });
+  };
+};
